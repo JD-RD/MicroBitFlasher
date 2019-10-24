de40b38a8840a59c0a6c31c3e123dbc074649efe
JD <jd@lafaceb.live>
2019-10-23 22:02:57 -0400

    10 MBs Update

--- MicroBitFlasher.sh
+++ MicroBitFlasher.sh
@@ -1,3 +1,48 @@
-diskutil rename "MICROBIT" "MB01";diskutil rename "MICROBIT" "MB02";diskutil rename "MICROBIT" "MB03"
+
 echo "Micro:Bit flashing process launched!"
-cp microbit-Dice-V1.hex /Volumes/MB01/;cp microbit-Dice-V1.hex /Volumes/MB02/;cp microbit-Dice-V1.hex /Volumes/MB03/
+function copy()
+{
+diskutil rename "MICROBIT" "MB01"
+sleep $1
+diskutil rename "MICROBIT" "MB02"
+sleep $1
+diskutil rename "MICROBIT" "MB03"
+sleep $1
+diskutil rename "MICROBIT" "MB04"
+sleep $1
+diskutil rename "MICROBIT" "MB05"
+sleep $1
+diskutil rename "MICROBIT" "MB06"
+sleep $1
+diskutil rename "MICROBIT" "MB07"
+sleep $1
+diskutil rename "MICROBIT" "MB08"
+sleep $1
+diskutil rename "MICROBIT" "MB09"
+sleep $1
+diskutil rename "MICROBIT" "MB10"
+sleep $1
+
+cp artbit-v2.3.hex /Volumes/MB01/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB02/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB03/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB04/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB05/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB06/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB07/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB08/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB09/
+sleep $1
+cp artbit-v2.3.hex /Volumes/MB010/
+sleep $1
+}
+copy $1
+echo "Micro:Bit flashing Done!"
--- README.md
+++ README.md
@@ -2,19 +2,20 @@
 
 Flash more than one BBC Micro:Bit at once
 
-## OSX (Hardcoded. Only working for 3 Micro:Bit)
-1. Mount 3 BBC Micro:Bit via USB ports (or USB hub)
+## OSX (Hardcoded. Only wWorking for 10 Micro:Bits)
+1. Mount 1 to 10 BBC Micro:Bits via USB
 2. Open Terminal (CMD+Space -> "Terminal")
 3. Type these commands to navigate to your MicroBitFlasher directory, and make the script executable
 ```bash
 cd /directory/with/MicroBitFlasher.sh
-chmod +x MicroBitFlasher.sh # only required if your file is not already executable
-./MicroBitFlasher.sh
+chmod +x MicroBitFlasher.sh
 ```
 4. Move the .hex you want to send to the MicroBitFlasher directory
-5. Edit each "microbit-Dice-V1.hex" strings in line 3 of MicroBitFlasher.sh with your .hex file name. Ex.:
+5. Edit each "artbit-V2.3.hex" strings in MicroBitFlasher.sh with your .hex file name. Ex.:
 ```bash
-cp YourFileNameHere.hex /Volumes/MB01/;cp YourFileNameHere.hex /Volumes/MB02/;cp YourFileNameHere.hex /Volumes/MB03/
+cp YourFileNameHere.hex /Volumes/MB01/
+cp YourFileNameHere.hex /Volumes/MB02/
+cp YourFileNameHere.hex /Volumes/MB03/
 ```
 6. Run the script
 ```bash
