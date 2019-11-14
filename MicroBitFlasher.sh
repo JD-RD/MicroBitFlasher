d37c2e94691366d47dbb7fcea3e44a3205f964b2
JD <jd@lafaceb.live>
2019-11-13 20:56:44 -0500

    filename input added

--- MicroBitFlasher.sh
+++ MicroBitFlasher.sh
@@ -1,48 +1,51 @@
 
+#!/bin/sh
+echo Please input .hex file name:
+read hex
 echo "Micro:Bit flashing process launched!"
 function copy()
 {
-diskutil rename "MICROBIT" "MB01"
-sleep $1
-diskutil rename "MICROBIT" "MB02"
-sleep $1
-diskutil rename "MICROBIT" "MB03"
-sleep $1
-diskutil rename "MICROBIT" "MB04"
-sleep $1
-diskutil rename "MICROBIT" "MB05"
-sleep $1
-diskutil rename "MICROBIT" "MB06"
-sleep $1
-diskutil rename "MICROBIT" "MB07"
-sleep $1
-diskutil rename "MICROBIT" "MB08"
-sleep $1
-diskutil rename "MICROBIT" "MB09"
-sleep $1
-diskutil rename "MICROBIT" "MB10"
-sleep $1
+    diskutil rename "MICROBIT" "MB01"
+    sleep $1
+    diskutil rename "MICROBIT" "MB02"
+    sleep $1
+    diskutil rename "MICROBIT" "MB03"
+    sleep $1
+    diskutil rename "MICROBIT" "MB04"
+    sleep $1
+    diskutil rename "MICROBIT" "MB05"
+    sleep $1
+    diskutil rename "MICROBIT" "MB06"
+    sleep $1
+    diskutil rename "MICROBIT" "MB07"
+    sleep $1
+    diskutil rename "MICROBIT" "MB08"
+    sleep $1
+    diskutil rename "MICROBIT" "MB09"
+    sleep $1
+    diskutil rename "MICROBIT" "MB10"
+    sleep $1
 
-cp artbit-v2.3.hex /Volumes/MB01/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB02/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB03/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB04/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB05/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB06/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB07/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB08/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB09/
-sleep $1
-cp artbit-v2.3.hex /Volumes/MB010/
-sleep $1
+    cp $hex /Volumes/MB01/
+    sleep $1
+    cp $hex /Volumes/MB02/
+    sleep $1
+    cp $hex /Volumes/MB03/
+    sleep $1
+    cp $hex /Volumes/MB04/
+    sleep $1
+    cp $hex /Volumes/MB05/
+    sleep $1
+    cp $hex /Volumes/MB06/
+    sleep $1
+    cp $hex /Volumes/MB07/
+    sleep $1
+    cp $hex /Volumes/MB08/
+    sleep $1
+    cp $hex /Volumes/MB09/
+    sleep $1
+    cp $hex /Volumes/MB010/
+    sleep $1
 }
 copy $1
 echo "Micro:Bit flashing Done!"
